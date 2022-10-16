2f87b7c861950a1391f9d6184f4398954ddc1fd2
ryu-min <SergeyR6@yandex.ru>
2022-10-16 15:45:09 +0300

    currect state before make changes

--- typer/src/gui/TyperWidget.cpp
+++ typer/src/gui/TyperWidget.cpp
@@ -4,6 +4,11 @@
 #include <QLabel>
 #include <QPushButton>
 
+#include <QPlainTextEdit>
+#include <QTextEdit>
+
+#include <QDebug>
+
 typer::gui::TyperWidget::TyperWidget(QWidget *parent)
     : QWidget( parent )
 {
@@ -20,12 +25,59 @@ void typer::gui::TyperWidget::buildForm()
 
     QVBoxLayout * labelLayout = new QVBoxLayout();
     labelLayout->addItem( new QSpacerItem(10, 10, QSizePolicy::Maximum, QSizePolicy::Maximum) );
-    labelLayout->addWidget( new QLabel("Тут будет Typer"));
+
+    QTextEdit * textEdit = new QTextEdit();
+    textEdit->setMaximumWidth(300);
+    textEdit->setMaximumHeight(60);
+
+    textEdit->setOverwriteMode(true);
+
+    textEdit->setTextColor(Qt::gray);
+    textEdit->append("text text text text");
+    textEdit->setTextColor(Qt::black);
+
+    connect( textEdit, &QTextEdit::textChanged, [textEdit](){
+
+        static bool inChanging = false;
+        if ( inChanging )
+        {
+            return;
+        }
+
+        inChanging = true;
+
+//        static int counter = 0;
+
+        QString placeHolder = "text text text text";
+        QString allText = textEdit->toPlainText();
+        QString realText = allText.right(allText.size() - placeHolder.size());
+
+        qDebug() << "clear";
+        textEdit->clear();
+
+
+
+        textEdit->setTextColor(Qt::black);
+        textEdit->append(realText);
+        qDebug() << "appen" << realText;
+
+        textEdit->setTextColor(Qt::gray);
+        textEdit->append(placeHolder);
+        qDebug() << "appen" << placeHolder;
+
+
+        inChanging = false;
+    });
+
+    labelLayout->addWidget( textEdit );
+
     labelLayout->addItem( new QSpacerItem(10, 10, QSizePolicy::Maximum, QSizePolicy::Maximum) );
 
 
     QHBoxLayout * mainLayout = new QHBoxLayout(this);
     mainLayout->addItem( new QSpacerItem(10, 10, QSizePolicy::Maximum, QSizePolicy::Maximum) );
+
+
     mainLayout->addLayout( labelLayout );
     mainLayout->addItem( new QSpacerItem(10, 10, QSizePolicy::Maximum, QSizePolicy::Maximum) );
 
