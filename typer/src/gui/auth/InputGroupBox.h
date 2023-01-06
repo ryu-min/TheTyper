fc7b03f14de15661448ee8c8e3c6be3a6e67de94
ryu-min <SergeyR6@yandex.ru>
2023-01-06 14:26:13 +0300

    remove style sheet from InputGroupBox

--- typer/src/gui/auth/InputGroupBox.h
+++ typer/src/gui/auth/InputGroupBox.h
@@ -16,20 +16,7 @@ namespace typer
                                bool passwordMode = false)
             {
                 QGroupBox * gb = new QGroupBox(title);
-                // @todo use colors from pallete
-                gb->setStyleSheet(" QGroupBox { "
-                                        "border: 1px solid #a3a3a3; "
-                                        "border-radius: 3px; "
-                                        "margin-top: 2ex;  /*leave space at the top for the title */ "
-                                        "font-size: 12px;"
-                                    "}"
-                                    "QGroupBox::title {"
-                                    "    subcontrol-origin: margin;"
-                                    "    subcontrol-position: top left;    /* position at the top center */"
-                                    "    padding: 5 0px;"
-                                    "    font-size: 13px;"
-                                    "    color: white;"
-                                    "}");
+
                 QVBoxLayout * gbLayout = new QVBoxLayout();
                 if ( passwordMode ) lineEdit->setEchoMode( QLineEdit::Password );
                 lineEdit->setPlaceholderText(tooltip);
