638bffd9a704809c4ca4fd9225bd057e6b4af81e
Ryumin Sergey <90983639+ryu-min@users.noreply.github.com>
2023-12-03 13:17:11 +0300

    remove unused sources

--- TyperDesktop/src/common/Network.h
+++ TyperDesktop/src/common/Network.h
@@ -9,18 +9,8 @@
 
 namespace typer
 {
-    /**
-     * move all of it from common to network namespace ?
-     */
     namespace common
     {
-
-        /// @todo think about combine all this error times
-        enum class AuthError {
-            CONNECTION_ERROR,
-            USER_INFO_ERROR
-        };
-
         enum class WordsRequestError
         {
             CONNECTION_ERROR,
@@ -33,27 +23,14 @@ namespace typer
             UNEXPECTED_RESULT
         };
 
-        using JwtToken = QString;
-
-        using AuthResult = Result<JwtToken, AuthError>;
-
         using WordsRequestResult = Result<QString, WordsRequestError>;
-
         using WordsType = QString;
-
         using WordsTypes = QStringList;
 
         using WordsTypesRequestResult = Result<WordsTypes, WordsTypesRequestError>;
 
-        AuthResult authUser( const AuthentificationInfo & info );
-
         WordsRequestResult requestWords( const WordsType & wordsType );
-
         WordsTypesRequestResult requestWordTypes();
-
-        JwtToken registerUser( const RegistrationInfo & info );
-
-        bool checkToken(JwtToken token);
 
     }
 }
